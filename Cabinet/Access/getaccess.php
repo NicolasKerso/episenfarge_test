0db13d390558b046df1f445feda4d6c91d529d0d
emma2442 <102244339+emma2442@users.noreply.github.com>
2024-02-16 14:40:55 +0100

    Update getaccess.php

--- Cabinet/Access/getaccess.php
+++ Cabinet/Access/getaccess.php
@@ -67,7 +67,85 @@ Password='".str_replace("'","`",$_POST['Password'])."'
 where Id_user=".$_POST['id_user']."	";
 echo '
 <center>
-<div id="passds"  style="color: green;font-size: 1em;font-weight: bold;">Votre mot de passe à bien modifier</div>      
+<div id="passds"  style="color: green;font-size: 1em;font-weight: bold;">Votre mot de passe à bien été modifié</div>      
+</center>';
+}
+
+
+
+mysqli_query($con,$queryACC);
+
+
+?><?php 
+include "../connexion.php";
+
+
+
+
+if(trim($_POST['type'])=="Insert")
+{
+
+
+
+ $queryACC="INSERT INTO users 
+
+
+(
+UserName,
+Password,
+Specialite,
+FonctionL,
+dateCreation
+)
+
+
+
+ VALUES (
+'".str_replace("'","`",$_POST['Utilisateur'])."',
+'".str_replace("'","`",$_POST['Password'])."',
+'".str_replace(",",".",$_POST['Specialite'])."',
+'".str_replace("'","`",$_POST['FonctionM'])."',
+'getdate()'
+
+
+
+)";
+}
+
+
+
+
+else if($_POST['type']=='Delete')
+{
+	 $queryACC="delete  from  users  where Id_user=".$_POST['Id_user']."";
+}
+
+
+else if($_POST['type']=='update')
+{
+
+	$queryACC="update users set 
+
+UserName='".str_replace("'","`",$_POST['Utilisateur'])."',
+Specialite='".str_replace("'","`",$_POST['Specialite'])."',
+FonctionL='".str_replace(",",".",$_POST['FonctionM'])."'
+where Id_user=".$_POST['Id_user']."	";
+
+
+
+}
+
+else if($_POST['type']=='UpdateM')
+{
+
+	$queryACC="update users set 
+
+Password='".str_replace("'","`",$_POST['Password'])."'
+
+where Id_user=".$_POST['id_user']."	";
+echo '
+<center>
+<div id="passds"  style="color: green;font-size: 1em;font-weight: bold;">Votre mot de passe à bien été modifié</div>      
 </center>';
 }
 
