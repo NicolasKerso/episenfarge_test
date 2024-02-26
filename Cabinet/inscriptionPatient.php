4e2462131c39a0042941c774f9c5bb5487474c0c
Noxuro <100783178+Noxuro@users.noreply.github.com>
2024-02-26 09:57:23 +0100

    Update inscriptionPatient.php

--- Cabinet/inscriptionPatient.php
+++ Cabinet/inscriptionPatient.php
@@ -169,7 +169,7 @@ if ($message) {
         <label for="date">Date de naissance:</label>
         <input type="date" placeholder="Date de naissance" name="dateNaissance" required>
         <label for="Sexe">Sexe:</label>
-        <select name="Sexe" required>
+        <select name="Sexe" id="Sexe" required>
             <option value="">Sélectionner le sexe</option>
             <option value="M">Homme</option>
             <option value="F">Femme</option>
