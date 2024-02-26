9923a3419f92b86cc1e5b8bcae8a2bd4ebb07490
khadijaouakti <71464180+khadijaouakti@users.noreply.github.com>
2024-02-26 12:22:49 +0100

    Update index.php

--- Cabinet/index.php
+++ Cabinet/index.php
@@ -31,7 +31,7 @@ if(isset($_SESSION['id_user']) && $_SESSION['Fonction'] === 'Patient'){;?>
     <div class="dashboard-main-wrapper">
         <div class="dashboard-header">
             <nav class="navbar navbar-expand-lg bg-white fixed-top">
-                 <a class="navbar-brand" style="color: #0f7d7f;" href="#">Bonjour <?php echo $_SESSION['Username'];?>
+                 <a class="navbar-brand" style="color: #0f7d7f;" href="#">Bonjour <?php echo $_SESSION['username'];?>
                 : <?php echo $_SESSION['Fonction'];?> </a>
                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
