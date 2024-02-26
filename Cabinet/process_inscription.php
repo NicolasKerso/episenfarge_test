3de57fb7799b859ea755f3dc091ccfdc9de02402
Noxuro <100783178+Noxuro@users.noreply.github.com>
2024-02-26 10:43:40 +0100

    Update process_inscription.php

--- Cabinet/process_inscription.php
+++ Cabinet/process_inscription.php
@@ -53,3 +53,196 @@ if ($message) {
     echo "<div class='error'>$message</div>";
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+
+    <style type="text/css">
+        html, body {
+            background: linear-gradient(120deg, #72e99f 0%, #8fd3f4 100%);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            flex-direction: column;
+        }
+
+        *{
+            font-family: 'Roboto', sans-serif;
+            box-sizing: border-box;
+        }
+
+        form {
+            width: 500px;
+            border: 2px solid #ccc;
+            padding: 30px;
+            background: #fff;
+            border-radius: 60px;
+            box-shadow: 0px 10px 20px rgba(0,0,0,0.19), 0px 6px 6px rgba(0,0,0,0.23);
+        }
+
+        h2 {
+            text-align: center;
+            margin-bottom: 40px;
+            color: #333;
+        }
+
+        input {
+            display: block;
+            border: 2px solid #ccc;
+            width: 95%;
+            padding: 10px;
+            margin: 10px auto;
+            border-radius: 15px;
+            outline: none;
+        }
+        label {
+            color: #666;
+            font-size: 18px;
+            padding: 10px;
+            border-radius: 30px;
+        }
+
+        button {
+            float: right;
+            background: #72e99f;
+            padding: 10px 15px;
+            color: #fff;
+            border-radius: 15px;
+            margin-right: 10px;
+            border: none;
+            cursor: pointer;
+            transition: background 0.5s;
+        }
+        button:hover{
+            background: #8fd3f4;
+        }
+        .error {
+            background: #F2DEDE;
+            color: #A94442;
+            padding: 10px;
+            width: 95%;
+            border-radius: 5px;
+            margin: 20px auto;
+        }
+
+        h1 {
+            text-align: center;
+            color: #fff;
+        }
+
+        a {
+            float: right;
+            background: #84fab0;
+            padding: 10px 15px;
+            color: #fff;
+            border-radius: 5px;
+            margin-right: 10px;
+            border: none;<style type="text/css">
+                                               html, body {
+                                                   background: linear-gradient(120deg, #72e99f 0%, #8fd3f4 100%);
+                                                   display: flex;
+                                                   justify-content: center;
+                                                   align-items: center;
+                                                   height: 100vh;
+                                                   flex-direction: column;
+                                               }
+
+        *{
+            font-family: 'Roboto', sans-serif;
+            box-sizing: border-box;
+        }
+
+        form {
+            width: 500px;
+            border: 2px solid #ccc;
+            padding: 30px;
+            background: #fff;
+            border-radius: 60px;
+            box-shadow: 0px 10px 20px rgba(0,0,0,0.19), 0px 6px 6px rgba(0,0,0,0.23);
+        }
+
+        h2 {
+            text-align: center;
+            margin-bottom: 40px;
+            color: #333;
+        }
+
+        input {
+            display: block;
+            border: 2px solid #ccc;
+            width: 95%;
+            padding: 10px;
+            margin: 10px auto;
+            border-radius: 15px;
+            outline: none;
+        }
+        label {
+            color: #666;
+            font-size: 18px;
+            padding: 10px;
+            border-radius: 30px;
+        }
+
+        button {
+            float: right;
+            background: #72e99f;
+            padding: 10px 15px;
+            color: #fff;
+            border-radius: 15px;
+            margin-right: 10px;
+            border: none;
+            cursor: pointer;
+            transition: background 0.5s;
+        }
+        button:hover{
+            background: #8fd3f4;
+        }
+        .error {
+            background: #F2DEDE;
+            color: #A94442;
+            padding: 10px;
+            width: 95%;
+            border-radius: 5px;
+            margin: 20px auto;
+        }
+
+        h1 {
+            text-align: center;
+            color: #fff;
+        }
+
+        a {
+            float: right;
+            background: #84fab0;
+            padding: 10px 15px;
+            color: #fff;
+            border-radius: 5px;
+            margin-right: 10px;
+            border: none;
+            text-decoration: none;
+            transition: background 0.5s;
+        }
+        a:hover{
+            background: #8fd3f4;
+        }
+
+        text-decoration: none;
+        transition: background 0.5s;
+        }
+        a:hover{
+            background: #8fd3f4;
+        }
+    </style>
+</head>
+
+
+
+
+
+<body>
+    <p>Vous etes bien inscrit! <a href="Authentification">Inscrivez-vous ici.</a></p>
+    
+</body>
+</html>
