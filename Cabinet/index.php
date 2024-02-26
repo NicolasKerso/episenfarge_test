0f512c178c424d929c02f13aa52f430dc821dd89
khadijaouakti <71464180+khadijaouakti@users.noreply.github.com>
2024-02-26 12:41:38 +0100

    Update index.php

--- Cabinet/index.php
+++ Cabinet/index.php
@@ -1,6 +1,6 @@
 <?php session_start();
 if(isset($_SESSION['id_user']) && $_SESSION['Fonction'] === 'Patient'){
-	$numSecu = $_SESSION['numSecu'];;?>
+	$numSecu = $_SESSION['numSecu'];?>
 <!doctype html>
 <html lang="en">
  
@@ -32,8 +32,7 @@ if(isset($_SESSION['id_user']) && $_SESSION['Fonction'] === 'Patient'){
     <div class="dashboard-main-wrapper">
         <div class="dashboard-header">
             <nav class="navbar navbar-expand-lg bg-white fixed-top">
-                 <a class="navbar-brand" style="color: #0f7d7f;" href="#">Bonjour <?php echo htmlspecialchars($numSecu);?>
-                : <?php echo $_SESSION['Fonction'];?> </a>
+                 <a class="navbar-brand" style="color: #0f7d7f;" href="#">Bonjour <?php echo htmlspecialchars($numSecu);?></a>
 	
                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
