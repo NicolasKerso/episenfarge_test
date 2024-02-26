7a0d54448c5c29adbef847dea8586f696bbdf1e9
khadijaouakti <71464180+khadijaouakti@users.noreply.github.com>
2024-02-26 11:40:47 +0100

    Update Inscription.php

--- Cabinet/Inscription.php
+++ Cabinet/Inscription.php
@@ -215,9 +215,6 @@ if ($message) {
         <input type="password" placeholder="Confirmation du mot de passe" name="confirmPasswordMedecin" required>
     </div>
     <div>
-        <a href="inscriptionMedecin.php">Medecin</a>
-    </div>
-    <div>
         <input type="submit" value="S'inscrire"/>
     </div>
     <p>Vous avez déjà un compte ? <a href="Authentification.php">Connectez-vous ici.</a></p>
