07b1b7c245ef9264e76fd34e32d693973c4196c1
boussouralisa <102244282+boussouralisa@users.noreply.github.com>
2024-02-27 01:06:14 +0100

    Update index2.php

--- Cabinet/index2.php
+++ Cabinet/index2.php
@@ -30,7 +30,8 @@ if(isset($_SESSION['id_user']) && $_SESSION['Fonction'] === 'Secretaire'){;?>
     <body onload="functionGetImageBody();">
     <div class="dashboard-main-wrapper">
         <div class="dashboard-header">
-            <nav class="navbar navbar-expand-lg bg-white fixed-top">
+            <nav class="navbar navbar-expand-lg" style="background-color: rgb(75, 157, 160); position: fixed; top: 0; width: 100%;"></nav>
+
                 <a class="navbar-brand" href="#"><img src="img/logo.png" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
 
 
