71ce63e4bb3e502389fb5e2b0acecf745f68fb37
hiendinh2001 <102244470+hiendinh2001@users.noreply.github.com>
2024-03-01 16:36:38 +0100

    Update process_inscription.php

--- Cabinet/process_inscription.php
+++ Cabinet/process_inscription.php
@@ -22,8 +22,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['fonction'])) {
         } 
         if ($_POST['fonction'] == "Patient") {
             $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            $stmt = $con->prepare("INSERT INTO patient (NumSecu, Nom_Pat, Prenom_Pat, DateNaissance, Sexe, Adresse, CodePostal, Ville, Telephone, Email, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            $stmt->bind_param("sssssssssss", $_POST['numSecu'], $_POST['nom'], $_POST['prenom'], $_POST['dateNaissance'], $_POST['sexe'], $_POST['adresse'], $_POST['codePostal'], $_POST['ville'], $_POST['telephone'], $_POST['email'], $hashedPassword);
+            $stmt = $con->prepare("INSERT INTO patient (NumSecu, Nom_Pat, Prenom_Pat, DateNaissance, Sexe, Adresse, CodePostal, Ville, Telephone, Email, Password,numerobracelet) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("ssssssssssss", $_POST['numSecu'], $_POST['nom'], $_POST['prenom'], $_POST['dateNaissance'], $_POST['sexe'], $_POST['adresse'], $_POST['codePostal'], $_POST['ville'], $_POST['telephone'], $_POST['email'], $hashedPassword, $_POST['numerobracelet']);
+        }
+        
+        if ($_POST['fonction'] == "Medecin" && $_POST['passwordMedecin'] !== $_POST['confirmPasswordMedecin']) {
+            throw new Exception("Les mots de passe ne correspondent pas.");
+        } 
+        if ($_POST['fonction'] == "Medecin") {
+            $hashedPasswordMedecin = password_hash($_POST['passwordMedecin'], PASSWORD_DEFAULT);
+            $stmt = $con->prepare("INSERT INTO medecin (NumCPS, Nom, Prenom, DateNaissance, Sexe, Adresse, CodePostal, Ville, Telephone, Email, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("sssssssssss", $_POST['numCPS'], $_POST['nomMedecin'], $_POST['prenomMedecin'], $_POST['dateNaissanceMedecin'], $_POST['sexeMedecin'], $_POST['adresseMedecin'], $_POST['codePostalMedecin'], $_POST['villeMedecin'], $_POST['telephoneMedecin'], $_POST['emailMedecin'], $hashedPasswordMedecin);
         }
       
         $stmt->execute();
@@ -33,14 +42,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['fonction'])) {
 
 
         $userId = $con->insert_id;
-
+        
+        $queryUpdateFirstLogin = $con->prepare("UPDATE patient SET first_login = 1 WHERE NumSecu = ?");
+        $queryUpdateFirstLogin->bind_param('s', $_POST['numSecu']);
+        $queryUpdateFirstLogin->execute();
         $con->commit();
-
+        
+        
 
         $_SESSION['userId'] = $userId;
         $_SESSION['fonction'] = "Secretaire";
         header('Location: index2.php');
-        exit();
+        //exit();
 
     } catch (Exception $e) {
 
@@ -52,6 +65,62 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['fonction'])) {
 if ($message) {
     echo "<div class='error'>$message</div>";
 }
+?>
+
+<?php
+
+// if (isset($_POST['btn'])){
+//     envoiMail($_POST['email'], $_POST['prenom']);
+//     header("location:Authentification.php");
+//  }//if
+
+envoiMail($_POST['email'], $_POST['prenom']);
+//header("location:Authentification.php");
+
+function envoiMail($destinationAddress, $destinationName){   
+    require "PHPMailer-master/src/PHPMailer.php"; 
+    require "PHPMailer-master/src/SMTP.php"; 
+    require 'PHPMailer-master/src/Exception.php'; 
+    $mail = new PHPMailer\PHPMailer\PHPMailer(true);//true:enables exceptions
+    try {
+        $mail->SMTPDebug = 0; //0,1,2: chế độ debug. khi chạy ngon thì chỉnh lại 0 nhé
+        $mail->isSMTP();  
+        $mail->CharSet  = "utf-8";
+        $mail->Host = 'smtp.gmail.com';  //SMTP servers
+        $mail->SMTPAuth = true; // Enable authentication
+        $mail->Username = 'thithuhien.dinh25@gmail.com'; // SMTP username
+        $mail->Password = 'eyxl gncd ennv ucmt';   // SMTP password
+        $mail->SMTPSecure = 'ssl';  // encryption TLS/SSL 
+        $mail->Port = 465;  // port to connect to                
+        $mail->setFrom('thithuhien.dinh25@gmail.com', 'DSI' ); 
+        $mail->addAddress($destinationAddress, $destinationName); //mail và tên người nhận  
+        $mail->isHTML(true);  // Set email format to HTML
+        $mail->Subject = 'Modification du mot de passe Medilab';
+        $mailContent = "
+        <p>Bonjour {$_POST['nom']} {$_POST['prenom']},<br></p>
+        <p>Vous venez d'être inscrit sur le site Medilab !!!<br></p>
+        <p>Le lien ci-dessous vous permet de vous connecter au site web :<br></p>
+        <p>Lien: <a href='https://episenfarge.ddns.net/test/Cabinet/Authentification.php'>https://episenfarge.ddns.net/test/Cabinet/Authentification.php</a><br></p>
+        <p>Votre identifiant : {$_POST['numSecu']}<br></p>
+        <p>Votre mot de passe provisoire : {$_POST['password']}<br></p>
+        <p>Après votre connexion, veuillez vous rendre dans l'onglet 'Modification de mot de passe' pour procéder au changement de mot de passe.<br></p>
+        <p>Bien cordialement,<br></p>
+        <p>DSI<br></p>
+        "; 
+        $mail->Body = $mailContent;
+        $mail->smtpConnect( array(
+            "ssl" => array(
+                "verify_peer" => false,
+                "verify_peer_name" => false,
+                "allow_self_signed" => true
+            )
+        ));
+        $mail->send();
+        echo "L'email a été envoyé";
+    } catch (Exception $e) {
+        echo "L'email ne peut pas être envoyé. Erreur : ", $mail->ErrorInfo;
+    }
+ }//function envoiMail
 ?>
 <!DOCTYPE html>
 <html>
@@ -140,7 +209,7 @@ if ($message) {
             border-radius: 5px;
             margin-right: 10px;
             border: none;
-        }
+}
         a:hover{
             background: #8fd3f4;
         }
